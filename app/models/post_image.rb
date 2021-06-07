c7fad054f0486b1d612cc5251ba0648316bcde19
Miwa-T <miwa951107@gmail.com>
2021-06-07 13:27:51 +0000

    create devise

---
+++ app/models/post_image.rb
@@ -0,0 +1,11 @@
+class PostImage < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  attachment :image
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  password: 'users/passwords',
+  registrations: 'users/registrations'
+}
 
   root 'homes#top'
+
+  namespace :public do
+    get 'users/unsubscribe' => 'users#unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw'
+    resources :users, only: [:show, :edit, :update]
+    resources :post_images do
+      resources :comments, only: [:create, :edit, :update, :destroy]
+      resources :favorites, only: [:create, :destroy]
+    end
+  end
+
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions',
+  password: 'admins/passwords',
+  registrations: 'admins/registrations'
+}
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
